bfb7a78a90f8e48c803de185419b6fbb34dea4ef
David-kirimi <muriiradavie@gmail.com>
2026-03-30 20:09:04 +0300

    fix: allow PUT and PATCH methods in CORS config

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors({
     origin: '*',
-    methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true
 }));
 
